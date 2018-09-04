041272925762d7e5ceb76e58c31cc561afbbf333
Dương Thanh Tân <nobudy55@gmail.com>
2018-09-04 16:48:33 +0700

    Update category.service.ts

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '../../node_modules/angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,8 @@ export class CategoryService {
   constructor(private db: AngularFireDatabase) {}
 
   getCategories() {
-    return this.db.list('/categories', {
-      query: {
-        orderByChild: 'name'
-      }
-    });
+    return this.db
+      .list('/categories', categories => categories.orderByChild('name'))
+      .snapshotChanges();
   }
 }
