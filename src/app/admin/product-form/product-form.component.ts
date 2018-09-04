2c25987acc5dd25213c0bea7a0923307881ff690
Dương Thanh Tân <nobudy55@gmail.com>
2018-09-04 16:55:41 +0700

    Update product-form.component.ts

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../category.service';
+import { ProductService } from '../../product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-form',
@@ -7,19 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product: Product = {};
+  id;
 
   constructor(
+    categoryService: CategoryService,
+    private productService: ProductService,
     private router: Router,
-    private categoryService: CategoryService,
-    private productService: ProductService
+    private route: ActivatedRoute
   ) {
     this.categories$ = categoryService.getCategories();
-  }
 
-  save(product) {
-    this.productService.create(product);
-    this.router.navigate(['/admin/products']);
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService
+        .get(this.id)
+        .pipe(take(1))
+        .subscribe(product => (this.product = product));
+    }
   }
 
   ngOnInit() {}
+
+  save(product) {
+    if (this.id) {
+      this.productService.update(this.id, product);
+    } else {
+      this.productService.create(product);
+    }
+    this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if (!confirm('Are you sure you want to delete this product')) {
+      return;
+    }
+    this.productService.delete(this.id);
+    this.router.navigate(['/admin/products']);
+  }
 }
