b4d36ef77219e4f16208fe73d96c951cd27d2ef5
Dương Thanh Tân <nobudy55@gmail.com>
2018-09-04 16:58:14 +0700

    Update admin-products.component.ts

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../product.service';
+import { map, filter } from 'rxjs/operators';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products;
+  filteredProducts;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    // this.filteredProducts$ = this.products$ = this.productService.getAll().pipe(
+    //   map(products => {
+    //     return products.map(product => ({
+    //       key: product.key,
+    //       value: product.payload.val()
+    //     }));
+    //   })
+    // );
+    this.productService
+      .getAll()
+      .subscribe(
+        products => (this.products = this.filteredProducts = products)
+      );
   }
 
+  search(query: string) {
+    if (query) {
+      this.filteredProducts = this.products.map(product => ({
+        key: product.key,
+        value: product.payload.val()
+      }));
+    }
+  }
+
+  ngOnInit() {}
 }
