3202b942258caaa13d67641a83815641474450a5
Dương Thanh Tân <nobudy55@gmail.com>
2018-09-04 16:50:52 +0700

    Update admin-auth-guard.service.ts

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import { UserService } from './user.service';
 import { AuthService } from './auth.service';
-import { CanActivate } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminAuthGuardService {
+export class AdminAuthGuardService implements CanActivate {
   constructor(private auth: AuthService, private userService: UserService) {}
 
-  canActivate(): Observable<boolean> {
-    return this.auth.appUser$.map(appUser => appUser.isAdmin);
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.auth.appUser$.pipe(map(appUser => appUser.isAdmin));
   }
 }
