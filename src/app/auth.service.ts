23e9ee59e37979a0eb0f1ba6dea3e93b23aef482
Dương Thanh Tân <nobudy55@gmail.com>
2018-09-04 16:48:58 +0700

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable, of } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from './user.service';
 import { AppUser } from './models/app-user';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Injectable } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
-import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +14,25 @@ export class AuthService {
   user$: Observable<firebase.User>;
 
   constructor(
-    private userService: UserService,
     private afAuth: AngularFireAuth,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
   ) {
     this.user$ = afAuth.authState;
+    this.user$.subscribe(user => {
+      if (user) {
+        // Save user info to db
+        userService.save(user);
+        // User get redirect back to the last page they're on when logged in
+        const returnUrl = localStorage.getItem('returnUrl');
+        this.router.navigateByUrl(returnUrl);
+      }
+    });
   }
 
   login() {
-    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
 
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
@@ -33,10 +43,13 @@ export class AuthService {
   }
 
   get appUser$(): Observable<AppUser> {
-    return this.user$.switchMap(user => {
-      if (user) return this.userService.get(user.uid);
-
-      return Observable.of(null);
-    });
+    return this.user$.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.userService.get(user.uid);
+        }
+        return of(null);
+      })
+    );
   }
 }
