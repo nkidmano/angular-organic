3c2657d54f61bf43492123e0a58691399d901f3c
Dương Thanh Tân <nobudy55@gmail.com>
2018-09-04 16:47:52 +0700

    Update user.service.ts

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import * as firebase from 'firebase';
 import { AppUser } from './models/app-user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class UserService {
     });
   }
 
-  get(uid: string): FirebaseObjectObservable<AppUser> {
-    return this.db.object('/users/' + uid);
+  get(uid: string): Observable<any> {
+    return this.db.object('/users/' + uid).valueChanges();
   }
 }
